12db2cc86efdb928c86cb32049e0928bc264aa91
franckDev21 <tutodevphp@gmail.com>
2022-08-31 22:48:09 +0100

    update app

--- src/Pages/All/index.tsx
+++ src/Pages/All/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Separator from "../../Atoms/Separator/Separator";
-import Text from "../../Atoms/Text/Text";
-import Card from "../../Melecules/Card/Card";
 import Banner from "../../Organismes/Banner/Banner";
 import Header from "../../Organismes/Header/Header";
 import OutService from "../../Organismes/OutService/OutService";
@@ -18,7 +15,7 @@ const All = () => {
 
       {/* Services */}
       <OutService className="mb-10" />
-      
+
     </AuthLayout>
   );
 };
